a5d055dc0f55f63b7d8cde6845eb38f426c505bb
crackcodecamp <justrock.akki@gmail.com>
2018-07-30 22:37:53 +0530

    Settings.py Updated

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4931kfmtmhyp)iwm8gx5v)!(*$n#vj_w+n=*yu2uiy!&w8pq02'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
